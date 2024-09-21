69157f0c2a266748afbdf807989230fed6a605dc
Blue <blue@bluecode.fr>
2024-09-21 00:24:49 -0700

    Add support for control relay

--- garix/__init__.py
+++ garix/__init__.py
@@ -1,5 +1,5 @@
 import click
-from gpiozero import Button
+from gpiozero import Button, OutputDevice
 import time
 import traceback
 import os
@@ -8,7 +8,7 @@ import json
 import subprocess
 
 from threading import Event, Thread, Lock
-from flask import Flask, Response
+from flask import Flask, Response, request
 
 
 app = Flask(__name__)
@@ -36,9 +36,16 @@ def apply_leds(state: list):
         with open(led, 'w') as fd:
             fd.write(state)
 
+class HttpException (RuntimeError):
+    def __init__(self, code: int, message: str):
+        self.code = code
+        self.message = message
+
 class StateMachine:
-    def __init__(self, pin: int, delay: float, open_leds: list, closed_leds: list, open_state: str, closed_state: str, hook: str):
+    def __init__(self, pin: int, delay: float, open_leds: list, closed_leds: list, open_state: str, closed_state: str, relay_pin: int, hook: str, state_change_timeout: float):
         self.event = Event()
+        self.state_change_event = Event()
+        self.state_change_mutex = Lock()
         self.mutex = Lock()
         self.last_transition = None
         self.delay = delay
@@ -50,6 +57,9 @@ class StateMachine:
         self.open_state = open_state
         self.closed_state = closed_state
         self.hook = hook
+        self.relay_pin = OutputDevice(relay_pin, active_high=True, initial_value=False) if relay_pin else None
+        self.state_change_timeout = state_change_timeout
+        self.error_state = False
 
     def map_state(self, state: bool) -> str:
         if state is None:
@@ -76,11 +86,49 @@ class StateMachine:
         with self.mutex:
             self.state = new_state
             self.last_transition = now
+            self.state_change_event.set()
 
         if self.state is True and self.closed_leds is not None:
             apply_leds(self.closed_leds)
         elif self.state is False and self.open_leds:
             apply_leds(self.open_leds)
+
+    def change_state(self, new_state):
+        if self.relay_pin is None:
+            raise HttpException(400, f'Relay pin not configured')
+
+        if new_state == self.open_state:
+            expected_state = True
+        elif new_state == self.closed_state:
+            expected_state = False
+        else:
+            raise HttpException(400, f'Invalid state: "{new_state}", expected "{self.open_state}" or "{self.closed_state}"')
+
+        with self.state_change_mutex:
+            with self.mutex:
+                if self.state == expected_state:
+                    return json.dumps({'state': new_state, 'ts': self.last_transition, 'transition_time': None})
+
+                if self.error_state:
+                    raise HttpException(500, f'Error state is set, refusing transition')
+
+                self.state_change_event.clear()
+
+                print(f'Activating relay. Target state: "{new_state}"')
+                self.relay_pin.on()
+                start_ts = time.time()
+
+                time.sleep(0.1)
+                self.relay_pin.off()
+
+        if not self.state_change_event.wait(self.state_change_timeout):
+            print(f'Timed out waiting for door to reach state "{expected_state}", setting error state')
+
+            self.error_state = True
+            raise HttpException(500, f'Timed out waiting for state: "{new_state}"')
+
+        end_ts = time.time() - start_ts
+        return json.dumps({'state': new_state, 'ts': self.last_transition, 'transition_time': end_ts})
 
     def run(self):
         try:
@@ -126,7 +174,7 @@ class StateMachine:
 
     def get_state(self):
         with self.mutex:
-            return self.map_state(self.state), self.last_transition
+            return self.map_state(self.state), self.last_transition, self.error_state
 
 def parse_leds_arg(arg: str) -> list:
     if arg is None:
@@ -151,7 +199,9 @@ def parse_leds_arg(arg: str) -> list:
 @click.option('--open-state', default='Opened')
 @click.option('--closed-state', default='Closed')
 @click.option('--hook', default=None)
-def live(pin: int, poll_delay: float, open_leds: str, closed_leds: str, open_state: str, closed_state: str, hook: str):
+@click.option('--relay-pin', default=None, type=int)
+@click.option('--transition_timeout', default=30, type=float)
+def live(pin: int, poll_delay: float, open_leds: str, closed_leds: str, open_state: str, closed_state: str, hook: str, relay_pin: int, transition_timeout: float):
 
 
     print('Running in live mode')
@@ -161,9 +211,22 @@ def live(pin: int, poll_delay: float, open_leds: str, closed_leds: str, open_sta
                       closed_leds=parse_leds_arg(closed_leds),
                       open_state=open_state,
                       closed_state=closed_state,
-                      hook=hook) as m:
+                      hook=hook,
+                      relay_pin=relay_pin,
+                      state_change_timeout=transition_timeout) as state_machine:
 
-        input('Press a key to exit')
+        while True:
+            key = input('Press "o" to open, "c" to close or any other key to exit')
+
+            try:
+                if key == 'o':
+                    print(state_machine.change_state(open_state))
+                elif key == 'c':
+                    print(state_machine.change_state(closed_state))
+                else:
+                    break
+            except HttpException as e:
+                print(f'Caught HttpException. Code: {e.code}, message: {e.message}')
 
 
 @cli.command()
@@ -176,7 +239,9 @@ def live(pin: int, poll_delay: float, open_leds: str, closed_leds: str, open_sta
 @click.option('--open-state', default='Opened')
 @click.option('--closed-state', default='Closed')
 @click.option('--hook', default=None)
-def serve(pin: int, address: str, port: int, poll_delay: float, open_leds: str, closed_leds: str, open_state: str, closed_state: str, hook: str):
+@click.option('--relay-pin', default=None, type=int)
+@click.option('--transition_timeout', default=30, type=float)
+def serve(pin: int, address: str, port: int, poll_delay: float, open_leds: str, closed_leds: str, open_state: str, closed_state: str, hook: str, relay_pin: int, transition_timeout: float):
 
     with StateMachine(pin=pin,
                       delay=poll_delay,
@@ -184,7 +249,9 @@ def serve(pin: int, address: str, port: int, poll_delay: float, open_leds: str, 
                       closed_leds=parse_leds_arg(closed_leds),
                       open_state=open_state,
                       closed_state=closed_state,
-                      hook=hook) as machine:
+                      hook=hook,
+                      relay_pin=relay_pin,
+                      state_change_timeout=transition_timeout) as machine:
 
         global state_machine
         state_machine = machine
@@ -193,18 +260,29 @@ def serve(pin: int, address: str, port: int, poll_delay: float, open_leds: str, 
 
         app.run(host=address, port=int(port))
 
+@app.route('/transition', methods=['POST'])
+def transition():
+    target_state = request.form.get('state')
+    if target_state is None:
+        'Missing state parameter', 400
+
+    return state_machine.change_state(target_state), 200
 
 @app.route('/json', methods=['GET'])
 def get_json():
-    state, ts = state_machine.get_state()
+    state, ts, error = state_machine.get_state()
 
-    return json.dumps({'state': state, 'ts': ts}), 200
+    return json.dumps({'state': state, 'ts': ts, 'error': error}), 200
 
 @app.route('/prometheus', methods=['GET'])
 def get_prometheus():
-    state, _= state_machine.get_state()
+    state, _, _= state_machine.get_state()
 
     return f'state {state}\n', 200
+
+@app.errorhandler(HttpException)
+def on_error(exception: HttpException):
+    return exception.message, exception.code
 
 def main():
     try:
